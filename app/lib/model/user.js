a677c70709fafe6a53bea33685b8aca4f6259583
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2024-12-20 02:41:04 -0800

    Delete app directory

--- app/lib/model/user.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const userModel = new mongoose.Schema({
-    email:String,
-    image:String,
-    name:String,
-});
-export const users = mongoose.models.users || mongoose.model("users",userModel);
